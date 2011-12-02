4c348e9ff5876a0c3df1bc978d7b06b02cda999b
Gaston Ramos <ramos.gaston@gmail.com>
2011-12-02 11:56:37 -0300

    - improve help message - load files using absolute path with File.expand_path

--- lib/arg_parser.rb
+++ lib/arg_parser.rb
@@ -2,6 +2,26 @@ class ArgParser
   def self.parse(args)
     options = OpenStruct.new
 
+    gen_img_help = <<-EOS
+   ImageTooth paints the faces of the tooth image on hash colors based.
+   The tooth's filename represents the faces painted,
+   The filename's characters are:
+
+   * w - White
+   * g - Green
+   * b - Blue
+   * n - Black
+   * r - Red
+   * t - To extract
+   * x - Extracted
+
+   and are ordered, begining on the upper face of the
+   the tooth and moving clockwise ending on the center face.
+
+   Example: imagetooth --image wrrgw.png --folder ./
+
+    EOS
+
     opts = OptionParser.new do |opts|
       opts.on("-a", "--all", "Generate all teeth images's combinations.") do |all|
         options.all = all
@@ -16,6 +36,7 @@ class ArgParser
       end
 
       opts.on_tail("-h", "--help", "Show this message") do
+        puts gen_img_help
         puts opts
 
         exit
